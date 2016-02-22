44a075a7e8c86e9847220d5c023a6dd3e279a5e0
YeFan <yefan@YeFandeMacBook-Pro.local>
2016-02-23 02:28:42 +0800

    mysql

--- LM/settings.py
+++ LM/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'LM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',#django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME':'LMTdb',
+        'USER':'root',
+        'PASSWORD':'6688565',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
