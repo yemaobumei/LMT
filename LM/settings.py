6a399fefe37eecde1ea9e1563c1ed078b626e1a7
YeFan <yefan@YeFandeMacBook-Pro.local>
2016-02-23 14:41:11 +0800

    dere

--- LM/settings.py
+++ LM/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
 
 MEDIA_URL = '/media/'
 
