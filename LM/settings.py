a21cc358deca8909008f3c90372724e4dcf677b6
YeFan <yefan@YeFandeMacBook-Pro.local>
2016-02-23 15:07:19 +0800

    deqw

--- LM/settings.py
+++ LM/settings.py
@@ -95,7 +95,8 @@ DATABASES = {
     }
 }
 """
-
+from sae._restful_mysql import monkey
+monkey.patch()
 from sae.const import (MYSQL_HOST, MYSQL_HOST_S,
     MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB
 )
